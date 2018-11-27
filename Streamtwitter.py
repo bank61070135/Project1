7e6cfd17db5196eb09444d25de83dad7421b5d07
patcharapuek <patcharapuek@outlook.co.th>
2018-11-27 23:44:55 +0700

    clean data

--- Streamtwitter.py
+++ Streamtwitter.py
@@ -3,6 +3,8 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 import csv
+import json
+import re
 
 consumer_key = "3II162EiHwgcNCSV17YW0Ykof"
 consumer_secret = "CZonbTz3tao9tZkQVVCvQscf5Yml0ohV3H2n16JYktg4bY73z4"
@@ -11,13 +13,15 @@ access_token_secret = "wI0pzHELnIIMzTZPLm4Pn7Gj01SVL5zniJaDPM2ZcW6SG"
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
 
 class MyListener(StreamListener):
     def on_data(self, data):
         print(data)
-        with open("tweets.csv", "a") as f:
-            writer = csv.writer(f)
-        return True
+        hashtags = [hashtag.lower() for hashtag in re.split('\s+', data) if len(hashtag)>0 and hashtag[0]=="#"]
+        with open('python.csv', 'a') as f:
+            f.write(str(hashtags))
+            f.write("\n")
     def on_error(self, status):
         print(status)
 
