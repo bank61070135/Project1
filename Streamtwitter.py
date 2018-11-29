39adb8de6013031f1f0c2ae04ae73d4fb06aee03
patcharapuek <patcharapuek@outlook.co.th>
2018-11-30 00:10:11 +0700

    Data streaming and get hashtags and count to dict

--- Streamtwitter.py
+++ Streamtwitter.py
@@ -4,6 +4,7 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 import csv
 import re
+import pandas as pd
 
 consumer_key = "3II162EiHwgcNCSV17YW0Ykof"
 consumer_secret = "CZonbTz3tao9tZkQVVCvQscf5Yml0ohV3H2n16JYktg4bY73z4"
@@ -14,9 +15,11 @@ auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
+dict_hashtags = {}
+
 class MyListener(StreamListener):
     def on_data(self, data):
-        print(data)
+        #print(data)
         try:
             regex_str = [
                 r'<[^>]+>',
@@ -31,15 +34,28 @@ class MyListener(StreamListener):
             tokens_re = re.compile(r'('+'|'.join(regex_str)+')', re.VERBOSE | re.IGNORECASE)
             def tokenize(s):
                 return tokens_re.findall(s)
-            hashtags = [hashtag for hashtag in tokenize(data) if len(hashtag)>1 and hashtag[0]=="#"]
+            hashtags = [hashtag.lower() for hashtag in tokenize(data) if len(hashtag)>1 and hashtag[0]=="#"]
+
+            for i in hashtags:
+                if i not in dict_hashtags:
+                    dict_hashtags[i] = [1]
+                else:
+                    dict_hashtags[i][0] += 1
+
+            df_hashtags =  pd.DataFrame.from_dict(dict_hashtags, orient='index', columns=['Count'])
+            df_hashtags = df_hashtags.sort_values(by='Count', ascending=False)
+            print(df_hashtags)
             with open('Hashtags.csv', 'a', encoding='utf8') as f:
-                f.write(str(hashtags))
+                f.write(str(dict_hashtags))
+                f.write("\n")
+                f.write(str(df_hashtags))
                 f.write("\n")
         except BaseException as e:
             print("Error on_data: %s" % str(e))
         return True
     def on_error(self, status):
         print(status)
+
 
 if __name__ == "__main__":
     stream = Stream(auth, MyListener())
