f91390b35bf350f51f16081ef57a73598ba4d680
patcharapuek <patcharapuek@outlook.co.th>
2018-11-30 14:44:38 +0700

    Data Streaming

--- Streamtwitter.py
+++ Streamtwitter.py
@@ -62,29 +62,3 @@ class MyListener(StreamListener):
 if __name__ == "__main__":
     stream = Stream(auth, MyListener())
     stream.filter(track=["#java"])
-            
-
-import tweepy
-from tweepy.streaming import StreamListener
-from tweepy import OAuthHandler
-from tweepy import Stream
-
-consumer_key = "3II162EiHwgcNCSV17YW0Ykof"
-consumer_secret = "CZonbTz3tao9tZkQVVCvQscf5Yml0ohV3H2n16JYktg4bY73z4"
-access_token = "1349083645-NoM2NjgwsSqPPrT3DN8PgKMa4VFV8VGpV7qVX1H"
-access_token_secret = "wI0pzHELnIIMzTZPLm4Pn7Gj01SVL5zniJaDPM2ZcW6SG"
-
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-
-class MyListener(StreamListener):
-    def on_data(self, data):
-        print(data)
-        return True
-    def on_error(self, status):
-        print(status)
-
-if __name__ == "__main__":
-    stream = Stream(auth, MyListener())
-    stream.filter(track=["#"])
-
