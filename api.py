44291c6b6a76babc63c32867c10f9b20fea5a9fa
patcharapuek <patcharapuek@outlook.co.th>
2018-12-16 14:57:14 +0700

    update

--- api.py
+++ api.py
@@ -1,12 +1,11 @@
 from flask import Flask
 from flask_restful import Resource, Api
-
 app = Flask(__name__)
-while True:
-    data = open('Hashtags.txt', 'r').read()
-    @app.route('/count', methods=['GET'])
+data = open('Hashtags.txt', 'r').read()
 
-    def get_count():
-        return data
-    if __name__ == '__main__':
-        app.run(debug=True)
+@app.route('/count', methods=['GET'])
+
+def get_count():
+    return data
+if __name__ == '__main__':
+    app.run(debug=True)
